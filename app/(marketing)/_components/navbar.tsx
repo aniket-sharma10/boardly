75d9e5c43715386d4f8651d0aed347086e932d5d
aniket sharma <sharmaaniket094@gmail.com>
2025-12-01 11:03:45 +0530

    add navbar

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,10 +1,23 @@
 import Logo from "@/components/logo";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export const Navbar = () => {
   return (
     <div className="fixed top-0 w-full h-14 px-4 border-b shadow-sm bg-white flex items-center">
-      <div className="md:max-w-screen-2xl mx-auto flex items-center justify-between">
-        <Logo />
+      <div className="md:max-w-screen-2xl mx-auto flex items-center justify-between w-full">
+        <div className="flex items-center gap-4">
+          <Logo />
+        </div>
+        <div className="gap-4 flex flex-1 justify-between md:flex-none">
+          <Button size={"sm"} variant={"outline"} asChild className="">
+            <Link href="/signin">Login</Link>
+          </Button>
+
+          <Button size={"sm"} asChild className="">
+            <Link href="/signup">Get Boardly for free</Link>
+          </Button>
+        </div>
       </div>
     </div>
   );
--- components/logo.tsx
+++ components/logo.tsx
@@ -3,13 +3,11 @@ import Link from "next/link";
 
 const Logo = () => {
   return (
-    <div>
-      <Link href="/">
-        <div className="hover:opacity-75 transition items-center hidden md:flex">
-          <Image src="/Boardly.svg" alt="logo" width={200} height={200} />
-        </div>
-      </Link>
-    </div>
+    <Link href="/">
+      <div className="hover:opacity-75 transition items-center hidden md:flex">
+        <Image src="/Boardly.svg" alt="logo" width={200} height={200} />
+      </div>
+    </Link>
   );
 };
 
