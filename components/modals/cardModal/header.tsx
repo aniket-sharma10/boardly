529ee4610a9f6e2cf129c3ca83d4ae82f3c5a648
Aniket Sharma <sharmaaniket094@gmail.com>
2026-04-19 19:53:23 +0530

    added description component in cards

--- components/modals/cardModal/header.tsx
+++ components/modals/cardModal/header.tsx
@@ -16,49 +16,49 @@ interface HeaderProps {
 }
 
 export const Header = ({ data }: HeaderProps) => {
-  const queryClient = useQueryClient()
-  const params = useParams()
+  const queryClient = useQueryClient();
+  const params = useParams();
   const inputRef = useRef<HTMLInputElement>(null);
   const [title, setTitle] = useState(data.title);
-  const {execute} = useAction(updateCard, {
+  const { execute } = useAction(updateCard, {
     onSuccess: (data) => {
-      queryClient.invalidateQueries({queryKey: ["card", data.id]})
-      toast.success(`Renamed to ${data.title}`)
-      setTitle(data.title)
-    }, 
+      queryClient.invalidateQueries({ queryKey: ["card", data.id] });
+      toast.success(`Renamed to ${data.title}`);
+      setTitle(data.title);
+    },
 
     onError: (error) => {
-      toast.error(error)
-    }
-  })
+      toast.error(error);
+    },
+  });
 
   const onBlur = () => {
     inputRef.current?.form?.requestSubmit();
-  }
+  };
 
   const onSubmit = (formData: FormData) => {
-    const title = formData.get("title") as string
-    const boardId = params.boardId as string
-    if(title === data.title) return;
+    const title = formData.get("title") as string;
+    const boardId = params.boardId as string;
+    if (title === data.title) return;
 
-    execute({id: data.id, title, boardId})
-  }
+    execute({ id: data.id, title, boardId });
+  };
 
   return (
     <div className="flex items-start gap-x-3 mb-6 w-full">
       <Layout className="h-5 w-5 mt-1 text-neutral-700" />
       <form action={onSubmit}>
         <FormInput
-        ref={inputRef}
-        onBlur={onBlur}
+          ref={inputRef}
+          onBlur={onBlur}
           id="title"
           defaultValue={title}
           className="font-semibold text-xl px-1 text-neutral-700 bg-transparent border-transparent relative -left-1.5 w-[95%] focus-visible:bg-white focus-visible:border-input mb-0.5 truncate"
         />
+        <p className="text-sm w-full text-muted-foreground">
+          in list <span className="underline">{data.list.title}</span>
+        </p>
       </form>
-      <p className="text-sm w-full text-muted-foreground">
-        in list <span className="underline">{data.list.title}</span>
-      </p>
     </div>
   );
 };
